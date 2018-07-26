318c3e1683e86421c2c999e954de18d82af547f4
marko <soul6reaver@gmail.com>
2018-07-26 23:46:42 +0200

    secyrity

---
+++ src/main/java/com/travel/agency/controllers/LoginController.java
@@ -0,0 +1,51 @@
+package com.travel.agency.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.travel.agency.entities.User;
+import com.travel.agency.services.UserService;
+import com.travel.agency.utils.BikeUtils;
+
+@Controller
+public class LoginController {
+	
+	@Autowired
+    private UserService korisnikService;
+	
+	@Autowired
+    private HttpSession session;
+	
+	@GetMapping("/login")
+    public String viewLogin() {
+        return "login/login";
+    }
+	
+	@PostMapping("login")
+	public String processLogin(HttpServletRequest request, Model model, User korisnik) {
+
+		korisnik = korisnikService.findByName(request.getParameter("username"), request.getParameter("password"));
+		
+		if (!BikeUtils.isEmpty(korisnik)) {
+			session.setAttribute("user", korisnik);
+			session.setAttribute("role", korisnik.getRole().getName());
+			return "redirect:/";
+		}	
+	
+		model.addAttribute("error", "Wrong Credentials");
+		return "login/login";
+	}
+	
+	@GetMapping("logout")
+	public String logout(Model model) {
+		session.invalidate();
+		model.addAttribute("logout", "Logut");
+		return "login/login";
+	}
+}
--- src/main/java/com/travel/agency/services/UserService.java
+++ src/main/java/com/travel/agency/services/UserService.java
@@ -3,5 +3,8 @@ package com.travel.agency.services;
 import com.travel.agency.entities.User;
 
 public interface UserService {
+	
 	public User findByUsername(String username);
+	
+	public User findByName(String username, String password);
 }
